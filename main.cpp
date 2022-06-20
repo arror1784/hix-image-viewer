9f30ea59b1460a41201a70b1e72ed77d0f3af039
arror1784 <arror1784@gmail.com>
2022-06-20 18:31:12 +0900

    simple code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include "base64.h"
 
 static void destroy(GtkWidget *widget, gpointer data)
 {
@@ -13,7 +14,15 @@ int main(int argc, char *argv[])
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
     
-    gtk_widget_show_all(window);
+    // gtk_window_fullscreen(GTK_WINDOW(window));
+
+    GtkWidget *image = gtk_image_new_from_file("/home/jsh/overwatch.jpeg");
+    // GIcon gicon ;
+    // gtk_image_get_gicon(image,)
+    gtk_window_resize(GTK_WINDOW(window),1440,2560);
+
+    gtk_container_add(GTK_CONTAINER(window), image);
+    gtk_widget_show_all(window);    
     
     gtk_main();
     
