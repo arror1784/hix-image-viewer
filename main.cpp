e38deb55f53dcbadc84c7beb470b444da4bb2b8f
arror1784 <arror1784@gmail.com>
2022-09-21 14:39:12 +0900

    cairo test

--- main.cpp
+++ main.cpp
@@ -33,14 +33,49 @@ struct messageBuf
     char mtext[256];
 };
 
-void ipc_thread(GtkWidget *image);
+void ipc_thread(GtkWidget *draw);
 int handle_error(const std::error_code& error);
 void allocate_file(const std::string& path);
 static void destroy(GtkWidget *widget, gpointer data);
 
-void cb(){
-    std::cout << "DRAWWWWWWWWWWWw" << std::endl;
+
+mio::ummap_sink rw_mmap;
+int size = 0;
+gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    guint width, height;
+    GdkRGBA color;
+    GtkStyleContext *context;
+
+    context = gtk_widget_get_style_context (widget);
+
+    width = gtk_widget_get_allocated_width (widget);
+    height = gtk_widget_get_allocated_height (widget);
+
+    if(!rw_mmap.is_open())
+        return FALSE;
+    if(size == 0)
+        return FALSE;
+
+    std::cout << "HELLO FUCKING WORLD"<< std::endl;
+    GdkPixbufLoader *loader = gdk_pixbuf_loader_new();
+    gdk_pixbuf_loader_write (loader,rw_mmap.begin(), size, NULL);
+    
+    size = 0;
+
+    GdkPixbuf *pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
+    gtk_widget_get_window(widget);
+    // gdk_cairo_create()
+    gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
+    cairo_paint(cr);
+    // cairo_fill (cr);
+    // cairo_destroy (cr);
+    gdk_pixbuf_loader_close(loader,NULL);
+
+    return FALSE;
 }
+
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);    
@@ -55,28 +90,28 @@ int main(int argc, char *argv[])
     gtk_widget_show_all(window);
 
 
-    GtkWidget *image = gtk_image_new ();
-    // g_signal_connect(window, "draw", G_CALLBACK(cb), NULL);
-
     GdkRectangle workarea = {0};
     gdk_monitor_get_workarea(
         gdk_display_get_primary_monitor(gdk_display_get_default()),
         &workarea);
-    
-    gtk_widget_set_size_request (image,workarea.width, workarea.height);
+
+    GtkWidget *drawing_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request (drawing_area,workarea.width, workarea.height);
+    // g_signal_connect (G_OBJECT (drawing_area), "draw",
+                        // G_CALLBACK (draw_callback), NULL);
 
     std::cout << workarea.width<<workarea.height << std::endl;
 
-    std::thread th(ipc_thread,image);
-
-    gtk_container_add(GTK_CONTAINER(window), image);
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
     gtk_widget_show_all(window);
+
+    std::thread th(ipc_thread,drawing_area);
     gtk_main();
 
     return 0;
 }
 
-void ipc_thread(GtkWidget *image){
+void ipc_thread(GtkWidget *draw){
 
     std::string mmppath = "/opt/capsuleFW/print/mmap";
 
@@ -86,7 +121,6 @@ void ipc_thread(GtkWidget *image){
 
     std::error_code error;
 
-    mio::ummap_sink rw_mmap;
     rw_mmap.map(mmppath, 0, mio::map_entire_file, error);
 
     key_t mqKey = msgget(115200,IPC_CREAT|0666);
@@ -95,6 +129,7 @@ void ipc_thread(GtkWidget *image){
         perror("msgget error : ");
         exit(0);
     }
+
 
     while(1){
         if (msgrcv( mqKey, &mybuf, sizeof(messageBuf), 1, 0) == -1){
@@ -110,23 +145,29 @@ void ipc_thread(GtkWidget *image){
             
             rapidjson::Value& sizeV = d["size"];
 
+            size = sizeV.GetInt64();
+
             std::cout << "image change " << mybuf.mtext << std::endl;
 
             guint width, height;
-            width = gtk_widget_get_allocated_width (image);
-            height = gtk_widget_get_allocated_height (image);
+            width = gtk_widget_get_allocated_width (draw);
+            height = gtk_widget_get_allocated_height (draw);
 
+            std::cout << "HELLO FUCKING WORLD"<< std::endl;
             GdkPixbufLoader *loader = gdk_pixbuf_loader_new();
-            gdk_pixbuf_loader_write (loader,rw_mmap.begin(), sizeV.GetInt(), NULL);
+            gdk_pixbuf_loader_write (loader,rw_mmap.begin(), size, NULL);
+            
+            // size = 0;
+
             GdkPixbuf *pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
+
+            auto cr = gdk_cairo_create(gtk_widget_get_window(draw));
+            gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
+            cairo_paint(cr);
+            
             gdk_pixbuf_loader_close(loader,NULL);
 
-            // pixbuf=gdk_pixbuf_scale_simple(pixbuf,width,height,GDK_INTERP_BILINEAR);
-
-            gtk_image_set_from_pixbuf(GTK_IMAGE(image),pixbuf);
-
-            gtk_widget_queue_draw(image);
-
+            // gtk_widget_queue_draw(draw);
         }
         
     }
