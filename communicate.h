bcb1f17448cb0d5a1fa177a675595a2d06a6347a
arror1784 <arror1784@gmail.com>
2022-09-22 14:03:07 +0900

    messge queue insert to communicate

--- communicate.h
+++ communicate.h
@@ -13,22 +13,28 @@
 #define  KEY_NUM   1152
 #define  MEM_SIZE  100000
 
-class Communicate : public Singleton<Communicate>{
+typedef struct
+{
+    long msg_type;
+    char mtext[256];
+}messageBuf;
 
-    struct messageBuf
-    {
-        long msg_type;
-        char mtext[256];
-    };
+class Communicate : public Singleton<Communicate>{
 
 public:
     explicit Communicate(){
+        _mqKey = msgget(115200,IPC_CREAT|0666);
+
+        if (_mqKey == -1){
+            perror("msgget error : ");
+        }
         _shmid = shmget((key_t)KEY_NUM, MEM_SIZE, IPC_CREAT | 0666);
         if(_shmid == -1) {
             std::cout << "shm open fail" << std::endl;
 
         }else{
-            std::cout << "open sucess sibal" << std::endl;
+            std::cout << "open sucess" << std::endl;
+
         }
         void *shmaddr;
         shmaddr = shmat(_shmid, (void *)0, 0);
@@ -39,21 +45,38 @@ public:
         }
         _addr = (uint8_t*)shmaddr;
     }
-    ~Communicate(){
-        if(!_addr)
-            return;
-        if(shmdt(_addr) == -1)
-        {
-            perror("Shmdt failed");
-            return;
-        }
+    void addData(uint8_t *data,int size){
+        // std::copy_n(data,size,ummap.begin());
+        SharedMemoryWrite(data,size);
+        std::string msg = "{\"cmd\":\"imageChanged\",\"size\":" + std::to_string(size) + "}";
+
+        sendMessage(msg);
     }
     void getData(uint8_t *data,int size){
         SharedMemoryRead(data,size);
     }
+    bool receiveMessage(messageBuf* buf,bool blocking){
+        if(msgrcv( _mqKey, buf, sizeof(messageBuf), 1, blocking ? 0 : IPC_NOWAIT) == -1)
+            return false;
+        return true;
+    }
     uint8_t* _addr = NULL;
 
 private:
+    void sendMessage(std::string msg){
+        messageBuf mybuf;
+        if(msg.length() > 255){
+            return;
+        }
+        std::fill_n(mybuf.mtext,256,0x00);
+        std::copy_n(msg.data(),msg.length(),mybuf.mtext);
+        mybuf.msg_type = 1;
+        if (msgsnd( _mqKey, &mybuf, sizeof(messageBuf), IPC_NOWAIT) == -1)
+        {
+            perror("msgsnd error : ");
+            // exit(0);
+        }   
+    }
     bool SharedMemoryWrite(uint8_t *shareddata, int size){
         if(!_addr)
             return false;
@@ -86,6 +109,7 @@ private:
     }
     std::error_code error;
 
+    key_t _mqKey;
     int _shmid;
 
 };
